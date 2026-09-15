24d3393864de98693539565744d62761d7b5161a
MadeleineGaribaldi <madeleinegaribaldi9@gmail.com>
2026-09-15 13:07:18 -0600

    Update ALT_SpaDES.R

--- ALT_SpaDES.R
+++ ALT_SpaDES.R
@@ -139,6 +139,7 @@ doEvent.ALT_SpaDES = function(sim, eventTime, eventType) {
 ### template initialization
 Init <- function(sim) {
   # # ! ----- EDIT BELOW ----- ! #
+  print("Init")
   siteParameters = merge(sim$siteInfo,sim$gParameters, by="Landcover")  #### Coding blitz check... do these need to be sim$
   
   months = data.table(month = 5:10)
@@ -154,6 +155,7 @@ Init <- function(sim) {
 ### template for your event1
 ALTestimation <- function(sim) {
   # ! ----- EDIT BELOW ----- ! #
+  print("Inside ALTestimation")
   ALTparameters2 <- copy(mod$ALTparameters)
   ALTparameters2[
     ,
