90862d6a547efad820915736949d1222e28adfeb
MadeleineGaribaldi <madeleinegaribaldi9@gmail.com>
2026-09-15 13:56:01 -0600

    Update ALT_SpaDES.R

--- ALT_SpaDES.R
+++ ALT_SpaDES.R
@@ -172,6 +172,7 @@ ALTestimation <- function(sim) {
   ]
   ALTparameters2 <- ALTparameters2[!is.na(ALT)]
   mod$ALTparameters <- ALTparameters2
+  print("leaving")
 
   # ! ----- STOP EDITING ----- ! #
   return(invisible(sim))
